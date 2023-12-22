ca64881572a0b97e8c172b2cadffeaae6b698a48
davidpenard <david@ut-biomet.org>
2023-12-22 18:30:17 +0900

    including seeds for bayesian optimization in runOpt.R

--- runOpt.R
+++ runOpt.R
@@ -3,34 +3,35 @@
 #
 # Description:
 # shortcut to run the results
-
+# library(rmarkdown)
+library(dplyr)
 library(dotenv)
 load_dot_env()
 source("src/utils.R")
 
 SelIntmode = 0 #selection intensity function (slope_i function)
 # if SelIntmode = 0, then SelIntSlope=0 and NewIndSlope=0
-BORep <- 1
-nDiffStartPopulation = 2
-# set.seed(1)
-# (SeedsForPopulationSetup = floor(runif(nDiffStartPopulation)*1e6))
-# digest::digest2int(as.character(seq(nDiffStartPopulation)))
-SeedsForPopulationSetup = round(1:nDiffStartPopulation * pi*1e6) %>%
-  rep(., each=BORep)
+nDiffStartPopulation = 10
+BORep <- 5
 
+SeedsForPopulationSetup <- round(1:nDiffStartPopulation * pi*1e6) %>%
+  rep(., each=BORep)
+SeedsForBO <- digest::digest2int(as.character(1:length(SeedsForPopulationSetup))) # set 1 different seed for each BO
 
 for (StartPop_k in 1:length(SeedsForPopulationSetup)){
-t <- format(Sys.time(), "%Y-%m-%d_%H-%M-%S")
+  t <- format(Sys.time(), "%Y-%m-%d_%H-%M-%S")
 
-PopSetupSeed = SeedsForPopulationSetup[StartPop_k]
-
-rmarkdown::render('runRepeatOpt.Rmd',
-                  output_file = paste0('runRepeatOpt_', t, "-initPop-",StartPop_k,'|',length(SeedsForPopulationSetup)),
-                  params = list(PopSetupSeed = PopSetupSeed,
-                                SelIntmode = SelIntmode,
-                                StartPop_k = StartPop_k))
-sendNotification("optimization finished !!!")
+  PopSetupSeed = SeedsForPopulationSetup[StartPop_k]
+  BOseed <- SeedsForBO[StartPop_k]
+  rmarkdown::render('runRepeatOpt.Rmd',
+                    output_file = paste0('runRepeatOpt_', t, "-initPop-",floor(StartPop_k/BORep),'|',
+                                         floor(length(SeedsForPopulationSetup)/BORep)),
+                    params = list(PopSetupSeed = PopSetupSeed,
+                                  SelIntmode = SelIntmode,
+                                  StartPop_k = floor(StartPop_k/BORep),
+                                  BOseed = BOseed))
+  # sendNotification("optimization finished !!!")
 }
 
-slackr::slackr_setup()
-slackr::slackr_upload(filename = "runRepeatOpt.md")
+# slackr::slackr_setup()
+# slackr::slackr_upload(filename = "runRepeatOpt.md")
